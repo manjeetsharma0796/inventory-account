de84d8b93111ba1400596e3695473ac24b59cdfa
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-05-10 22:26:20 +0530

    Added test cases and covered all possible exit points

--- src/inventory.js
+++ src/inventory.js
@@ -2,37 +2,37 @@ class Inventory {
   // #inventory
 
   constructor() {
-    this.inventory = {};  
+    this.inventory = {};
   };
 
   stock(item, quantity) {
-    if(this.inventory[item] === undefined) {
-      this.inventory[item] = quantity;
+    if (item in this.inventory) {
+      this.inventory[item] += quantity;
       return;
     }
 
-    this.inventory[item] += quantity;
+    this.inventory[item] = quantity;
   };
-  
+
   destock(item, quantity) {
-    this.inventory[item] -= quantity;
-  }
-  
+    if (this.inventory[item] >= quantity) {
+      this.inventory[item] -= quantity;
+      return;
+    }
+
+    return 'Quantity limit exceeded or does not exist';
+  };
+
   loadItems(stockList) {
-    for(const stock of Object.entries(stockList)) {
+    for (const stock of Object.entries(stockList)) {
       const [item, quantity] = stock;
       this.stock(item, quantity);
     }
   };
 
   value() {
-    return this.inventory;
+    return { ...this.inventory };
   };
 };
 
-// class Account {
-//   constructor(location, inventory) {
-//     this[location] = inventory;
-//   }
-// }
 exports.Inventory = Inventory;
--- test/inventory-test.js
+++ test/inventory-test.js
@@ -3,31 +3,57 @@ const { deepStrictEqual } = require('assert');
 const { Inventory } = require('../src/inventory.js');
 
 describe('inventory', function () {
-  it('Should create an empty inventory of ICU', function () {
-    const ICU = new Inventory();
+  describe('value', function () {
+    it('Should have an empty inventory of department as nothing is added', function () {
+      const ICU = new Inventory();
 
-    deepStrictEqual(ICU.value(), {});
+      deepStrictEqual(ICU.value(), {});
+    });
   });
 
-  it('Should stock an item with quantity in ICU inventory', function () {
-    const ICU = new Inventory();
-    ICU.stock('syline', 30)
+  describe('stock', function () {
+    it('Should stock an item with quantity', function () {
+      const ICU = new Inventory();
+      ICU.stock('syline', 30);
 
-    deepStrictEqual(ICU.value(), { syline: 30 });
+      deepStrictEqual(ICU.value(), { syline: 30 });
+    });
+
+    it('Should increase the quantity of an existing item', function () {
+      const ICU = new Inventory();
+      ICU.stock('ibuprofen', 1);
+
+      deepStrictEqual(ICU.value(), { ibuprofen: 1 });
+
+      ICU.stock('ibuprofen', 2);
+      deepStrictEqual(ICU.value(), { ibuprofen: 3 });
+    });
   });
 
-  it('Should destock an item with given quantity in ICU inventory', function () {
-    const ICU = new Inventory();
-    ICU.stock('syline', 30);
-    ICU.destock('syline', 10);
+  describe('destock', function () {
+    it('Should destock an item, when quantity is less or equal to its stock', function () {
+      const ICU = new Inventory();
+      ICU.stock('syline', 30);
+      ICU.destock('syline', 10);
 
-    deepStrictEqual(ICU.value(), { syline: 20 });
+      deepStrictEqual(ICU.value(), { syline: 20 });
+    });
+
+    it('Should not destock an item, when quantity is greater than its stock', function () {
+      const ICU = new Inventory();
+      ICU.stock('syline', 30);
+      ICU.destock('syline', 40);
+
+      deepStrictEqual(ICU.value(), { syline: 30 });
+    });
   });
 
-  it('Should read and load invnetory with set of item and quantity', function () {
-    const ICU = new Inventory();
-    ICU.loadItems({ para: 10, campol: 40 });
+  describe('loadItems', function () {
+    it('Should read and load invnetory with set of item and quantity', function () {
+      const ICU = new Inventory();
+      ICU.loadItems({ para: 10, campol: 40 });
 
-    deepStrictEqual(ICU.value(), { para: 10, campol: 40 });
-  })
+      deepStrictEqual(ICU.value(), { para: 10, campol: 40 });
+    });
+  });
 });
